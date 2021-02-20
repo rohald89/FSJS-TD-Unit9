419f64503b88229132a1cbeff5377408c6999a8d
Rohald van Merode <rohald_89@live.nl>
2021-02-20 17:26:03 +0100

    created models for course and user

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 // load modules
 const express = require('express');
 const morgan = require('morgan');
+const { sequelize } = require('./models');
 
 // variable to enable global error logging
 const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
@@ -38,6 +39,16 @@ app.use((err, req, res, next) => {
     error: {},
   });
 });
+
+// Test the database connection.
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+})();
 
 // set our port
 app.set('port', process.env.PORT || 5000);
